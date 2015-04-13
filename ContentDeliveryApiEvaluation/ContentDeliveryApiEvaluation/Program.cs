b9b917ab4d2f1d743b6e977a50ea7d8d16446a2e
Joe McGrath <mcgrathjoe@gmail.com>
2015-04-13 13:09:49 +0100

    Added hello world, and WebAPI Client NuGet package

--- ContentDeliveryApiEvaluation/ContentDeliveryApiEvaluation/Program.cs
+++ ContentDeliveryApiEvaluation/ContentDeliveryApiEvaluation/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ContentDeliveryApiEvaluation
+﻿using System;
+
+namespace ContentDeliveryApiEvaluation
 {
   class Program
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("This is to test getting content via the Contentful Content Delivery API.");
+
+      
+
+      Console.Read();
     }
   }
 }
