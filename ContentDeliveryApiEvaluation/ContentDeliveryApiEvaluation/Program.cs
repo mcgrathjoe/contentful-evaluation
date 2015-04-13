bc467adb0b815c21c4be307ad046d9b800ea9dab
Joe McGrath <mcgrathjoe@gmail.com>
2015-04-13 13:18:42 +0100

    Getting JSON from Contentful example space

--- ContentDeliveryApiEvaluation/ContentDeliveryApiEvaluation/Program.cs
+++ ContentDeliveryApiEvaluation/ContentDeliveryApiEvaluation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ContentDeliveryApiEvaluation
 {
@@ -8,7 +10,20 @@ namespace ContentDeliveryApiEvaluation
     {
       Console.WriteLine("This is to test getting content via the Contentful Content Delivery API.");
 
-      
+      using (var client = new HttpClient())
+      {
+        client.BaseAddress = new Uri("https://cdn.contentful.com/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+        var response = client.GetAsync("spaces/cfexampleapi?access_token=b4c0n73n7fu1").Result;
+        if (response.IsSuccessStatusCode)
+        {
+          var json = response.Content.ReadAsStringAsync().Result;
+          Console.WriteLine(json);
+        }
+      }
 
       Console.Read();
     }
